083e2092646349a43e50e9f64651c20cafcf66af
User <brn.quinta@gmail.com>
2025-10-09 13:41:04 -0300

    Refactor: Retirei metodos do popupimage

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -4,32 +4,17 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(selector) {
     super(selector);
-    this._popupImage = this.popup.querySelector(".image-popup__photo");
 
-    // Cria um handler ESC específico para esta instância
-    this._handleImageEscClose = (event) => {
-      if (event.key === "Escape") {
-        this.close();
-      }
-    };
+    // Seleciona a imagem dentro do popup
+    this._popupImage = this.popup.querySelector(".image-popup__photo");
   }
 
   open({ name, link }) {
+    // Define a URL da imagem e o texto alternativo
     this._popupImage.src = link;
     this._popupImage.alt = name;
 
-    // Remove qualquer listener anterior e adiciona o específico
-    document.removeEventListener("keydown", this._handleEscClose);
-    document.removeEventListener("keydown", this._handleImageEscClose);
-
-    this.popup.classList.add("overlay--active");
-    this.popup.classList.add("visible");
-
-    document.addEventListener("keydown", this._handleImageEscClose);
-  }
-
-  close() {
-    super.close();
-    document.removeEventListener("keydown", this._handleImageEscClose);
+    // Chama o método open() genérico da classe pai
+    super.open();
   }
 }
